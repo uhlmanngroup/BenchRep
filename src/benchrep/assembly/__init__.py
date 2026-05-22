eb0d350d3250c7ddb279d9b17a3cfd5438a85142
akmhamid <akmhamid4@gmail.com>
2026-05-22 13:31:29 +0200

    added assembly config utils for yaml to dict, and exports

--- src/benchrep/assembly/__init__.py
+++ src/benchrep/assembly/__init__.py
@@ -1,3 +1,17 @@
 from benchrep.assembly.config import load_config
+from benchrep.assembly.config_utils import (
+    get_optional_section,
+    get_required_section,
+    get_required_value,
+    normalize_name,
+    require_mapping,
+)
 
-__all__ = ["load_config"]
+__all__ = [
+    "load_config",
+    "require_mapping",
+    "get_required_section",
+    "get_optional_section",
+    "get_required_value",
+    "normalize_name",
+]
--- src/benchrep/assembly/builders/__init__.py
+++ src/benchrep/assembly/builders/__init__.py
@@ -1,3 +1,9 @@
+from benchrep.assembly.builders.data_builder import build_datamodule
+from benchrep.assembly.builders.model_builder import build_model
 from benchrep.assembly.builders.optimizer_builder import build_optimizer_factory
 
-__all__ = ["build_optimizer_factory"]
+__all__ = [
+    "build_datamodule",
+    "build_model",
+    "build_optimizer_factory",
+]
